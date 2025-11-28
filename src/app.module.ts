428630b0740eff7f8a1cb9d2e0b8400236099df6
patrickfv <patrick.fv@outlook.com>
2025-11-28 20:11:01 -0300

    add postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '9qasp5v56q8ckkf5dc.leapcellpool.com',
+      port: 6438,
+      username: 'sovbxmhfiinyuxosrpyi',
+      password: 'scqpwqjhnyjbpuujhcpnyewlmvardv',
+      database: 'uoupvldsiqknrvnyvnjt',
+      entities: [__dirname+'/**/*.entity{.ts,.js'],
+      synchronize: true,
+      ssl: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
